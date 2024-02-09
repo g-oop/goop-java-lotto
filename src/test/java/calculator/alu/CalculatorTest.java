56c8f40dd339a2ab2a190668b5472d2b6342ee56
최지훈 <boy0516@naver.com>
2024-02-10 03:06:52 +0900

    test: 피드백 반영 수정
    - alu 삭제 반영

--- src/test/java/calculator/alu/CalculatorTest.java
+++ src/test/java/calculator/alu/CalculatorTest.java
@@ -7,45 +7,43 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.*;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.*;
 
 @DisplayName("도메인 - Calculator 테스트")
 class CalculatorTest {
 
-    static Stream<Arguments> methIngredients() {
+    static Stream<Arguments> mathIngredients() {
         return Stream.of(
-            Arguments.of(List.of("1", "+", "2").stream().map(MethIngredient::new).toList(), 3),
-            Arguments.of(List.of("2", "-", "1").stream().map(MethIngredient::new).toList(), 1),
-            Arguments.of(List.of("2", "*", "2").stream().map(MethIngredient::new).toList(), 4),
-            Arguments.of(List.of("4", "/", "2").stream().map(MethIngredient::new).toList(), 2),
-            Arguments.of(List.of("4", "+", "2", "-", "2", "*", "2", "/", "2").stream().map(MethIngredient::new).toList(), 4)
+            Arguments.of(List.of("1", "+", "2").stream().map(MathIngredient::new).toList(), 3),
+            Arguments.of(List.of("2", "-", "1").stream().map(MathIngredient::new).toList(), 1),
+            Arguments.of(List.of("2", "*", "2").stream().map(MathIngredient::new).toList(), 4),
+            Arguments.of(List.of("4", "/", "2").stream().map(MathIngredient::new).toList(), 2),
+            Arguments.of(List.of("4", "+", "2", "-", "2", "*", "2", "/", "2").stream().map(MathIngredient::new).toList(), 4)
         );
     }
 
-    static Stream<Arguments> invalidMethIngredients() {
+    static Stream<Arguments> invalidMathIngredients() {
         return Stream.of(
-            Arguments.of(List.of("1", "+", "*").stream().map(MethIngredient::new).toList(), "*"),
-            Arguments.of(List.of("2", "+", "1", "3").stream().map(MethIngredient::new).toList(), "3")
+            Arguments.of(List.of("1", "+", "*").stream().map(MathIngredient::new).toList(), "*")
         );
     }
 
     @DisplayName("계산 결과 테스트")
     @ParameterizedTest
-    @MethodSource("methIngredients")
-    void calculate(List<MethIngredient> methIngredients, int expected) {
+    @MethodSource("mathIngredients")
+    void calculate(List<MathIngredient> mathIngredients, int expected) {
         Calculator calculator = new Calculator();
-        int result = calculator.calculate(methIngredients);
+        int result = calculator.calculate(mathIngredients);
         assertThat(result).isEqualTo(expected);
     }
 
     @DisplayName("무효값 계산 예외 테스트")
     @ParameterizedTest
-    @MethodSource("invalidMethIngredients")
-    void calculate_유효하지_않은_수식_예외처리(List<MethIngredient> methIngredients, String expected) {
+    @MethodSource("invalidMathIngredients")
+    void calculate_유효하지_않은_수식_예외처리(List<MathIngredient> mathIngredients, String expected) {
         Calculator calculator = new Calculator();
         assertThatIllegalArgumentException().isThrownBy(
-            () -> calculator.calculate(methIngredients)
+            () -> calculator.calculate(mathIngredients)
         ).withMessage("유효하지 않은 인자 : " + expected);
     }
 }
--- src/test/java/calculator/alu/MathIngredientTest.java
+++ src/test/java/calculator/alu/MathIngredientTest.java
@@ -4,18 +4,16 @@ import java.util.List;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
-import static org.junit.jupiter.api.Assertions.*;
 
-@DisplayName("도메인 - MethIngredient 테스트")
-class MethIngredientTest {
+@DisplayName("도메인 - MathIngredient 테스트")
+class MathIngredientTest {
 
-    static Stream<Arguments> invalidMethIngredients() {
+    static Stream<Arguments> invalidMathIngredients() {
         return Stream.of(
             Arguments.of(List.of("2", " ", "4"), "blank")
         );
@@ -23,10 +21,10 @@ class MethIngredientTest {
 
     @DisplayName("생성자 유효성 검사 테스트")
     @ParameterizedTest
-    @MethodSource("invalidMethIngredients")
-    void initMethIngredient_무효값_예외처리(List<String> methIngredients, String expected) {
+    @MethodSource("invalidMathIngredients")
+    void initMathIngredient_무효값_예외처리(List<String> mathIngredients, String expected) {
         assertThatIllegalArgumentException().isThrownBy(
-            () -> methIngredients.stream().map(MethIngredient::new).toList()
+            () -> mathIngredients.stream().map(MathIngredient::new).toList()
         ).withMessage("유효하지 않은 인자 : " + expected);
     }
 }
